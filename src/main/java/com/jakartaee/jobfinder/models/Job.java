9235ce276e9a19b6172eafd4aa6053935feef082
Aubrey17 <aubreymapaile@gmail.com>
2026-05-02 00:05:43 +0200

    refactor: rename entity to models and util to utils

--- src/main/java/com/jakartaee/jobfinder/models/Job.java
+++ src/main/java/com/jakartaee/jobfinder/models/Job.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
 
-import com.jakartaee.jobfinder.util.SalaryFormatter;
+import com.jakartaee.jobfinder.utils.SalaryFormatter;
 
 @Entity
 @Table(name = "jobs")
